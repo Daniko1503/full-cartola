3bf3704d28769b6d93c6a192a63f9c6dc0d7fb81
Daniko1503 <drs_nico@hotmail.com>
2022-12-17 10:30:38 -0300

    inicial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Daniko1503/full-cartola
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
---
+++ internal/domain/entity/match_action.go
@@ -0,0 +1,27 @@
+package entity
+
+import "github.com/google/uuid"
+
+type GameAction struct {
+	ID         string
+	PlayerID   string
+	PlayerName string
+	TeamID     string
+	Minute     int
+	Action     string
+	Score      int
+}
+
+func NewGameAction(playerID string, minute int, action string, score int, teamID string) *GameAction {
+	return &GameAction{
+		ID:       uuid.New().String(),
+		PlayerID: playerID,
+		Minute:   minute,
+		TeamID:   teamID,
+		Action:   action,
+		Score:    score,
+	}
+}
+
+/*Footer
+© 2022 GitHub, I*/
